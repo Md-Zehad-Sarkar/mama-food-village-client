84a14dfd084092ef1fdc2728c6762c869a3aed80
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-05-09 19:56:41 +0600

    swiper slider added

--- src/components/UI/about/AboutSlider.tsx
+++ src/components/UI/about/AboutSlider.tsx
@@ -1,60 +1,80 @@
 "use client";
-
 import { useGetAllReviewsQuery } from "@/redux/api/reviews/reviewsApi";
 import Ratings from "@/utils/Ratings";
 import { Box, Typography } from "@mui/material";
 import Image from "next/image";
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Pagination, Navigation } from "swiper/modules";
 
 const AboutSlider = () => {
   const { data, isLoading } = useGetAllReviewsQuery(undefined);
   const reviews = data?.data;
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    speed: 100,
-    autoplaySpeed: 5000,
-    cssEase: "linear",
-    slidesToShow: 2,
-    slidesToScroll: 1,
-  };
   return (
-    <div>
-      <Slider {...settings}>
+    <>
+      <Swiper
+        slidesPerView={1}
+        pagination={{
+          type: "progressbar",
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
         {reviews?.map((review: any) => (
-          <Box
-            key={review?._id}
-            sx={{
-              bgcolor: "#FFFFFF",
-              padding: "5px 15px",
-              boxShadow: 2,
-              height: 350,
-              borderRadius: 5,
-            }}
-          >
-            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-              <Box sx={{ display: "flex", gap: 1 }}>
-                <Image
-                  src={review?.profile_image}
-                  alt={review?.profile_image}
-                  width={70}
-                  height={70}
-                  style={{ borderRadius: 50 }}
-                />
+          <SwiperSlide key={review?._id}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                maxWidth: "600px",
+                width: "100%",
+                margin: "0 auto",
+                paddingY:'10px'
+              }}
+            >
+              <Image
+                src={review?.profile_image}
+                alt={review?.profile_image}
+                width={70}
+                height={70}
+                style={{ borderRadius: 10, marginBottom: "5px" }}
+              />
+              <Ratings value={4} />
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  maxWidth: "600px",
+                  width: "100%",
+                  margin: "0 auto",
+                  marginTop: "10px",
+                  paddingX: { xs: "30px" },
+                }}
+              >
+                <Typography mt={2}>
+                  {review?.review.slice(0, 200)}...
+                </Typography>
                 <Box>
-                  <Typography>{review?.name}</Typography>
+                  <Typography fontWeight={600} mt={2}>
+                    {review?.name}
+                  </Typography>
                   <Typography>{review?.designation || "CEO"}</Typography>
                 </Box>
               </Box>
-              <Ratings value={4} />
             </Box>
-            <Typography mt={2}>{review?.review.slice(0, 200)}...</Typography>
-          </Box>
+          </SwiperSlide>
         ))}
-      </Slider>
-    </div>
+        {/* <SwiperSlide>Slide 1</SwiperSlide> */}
+      </Swiper>
+    </>
   );
 };
 
