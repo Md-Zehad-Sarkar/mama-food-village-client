cdf6f236615439072ae8a52b68e1e6c023f31773
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-05-16 18:30:04 +0600

    card style fixed for shop items

--- src/components/UI/Shop/ShopItems.tsx
+++ src/components/UI/Shop/ShopItems.tsx
@@ -11,53 +11,54 @@ const ShopItems = () => {
   const shopItems = data?.data;
   console.log(shopItems);
   return (
-    <Box mt={8}>
-      <Grid container spacing={3}>
+    <Box mt={10}>
+      <Grid container spacing={4}>
         {shopItems?.map((item: any) => (
-          <Grid
-            item
-            xs={12}
-            md={6}
-            lg={4}
-            key={item?._id}
-            sx={{ border:'1px solid', borderRadius: "15px",mx:'auto' }}
-          >
-            <Box>
-              <Image
-                src={item?.image}
-                alt="food items"
-                width={400}
-                height={200}
-                style={{width:'200px', height:'200px'}}
-              />
-            </Box>
-            <Box sx={{ borderBottom: "1px solid" }}></Box>
-            <Box>
-              <Ratings value={5} />
-              <Typography variant="h6" component={"h5"} fontWeight={600}>
-                {item?.name}
-              </Typography>
-              <Typography variant="h6" component={"h6"}>
-                {item?.description.slice(0, 40)}...
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  padding:'5px'
-                }}
-              >
-                <Typography
-                  variant="h5"
-                  component={"h4"}
-                  sx={{ color: "primary.main" }}
+          <Grid item xs={12} md={6} lg={4} key={item?._id} spacing={1}>
+            <Box
+              sx={{
+                border: "1px solid",
+                borderRadius: "15px",
+                mx: "auto",
+              }}
+            >
+              <Box>
+                <Image
+                  src={item?.image}
+                  alt="food items"
+                  width={200}
+                  height={200}
+                  style={{ width: "150px", height: "150px", margin: "auto" }}
+                />
+              </Box>
+              <Box sx={{ borderBottom: "1px solid" }}></Box>
+              <Box sx={{ bgcolor: "#F3f3f3", padding: 2,borderBottomLeftRadius:'15px',borderBottomRightRadius:'15px' }}>
+                <Ratings value={5} />
+                <Typography variant="h6" component={"h5"} fontWeight={600}>
+                  {item?.name}
+                </Typography>
+                <Typography variant="h6" component={"h6"}>
+                  {item?.description.slice(0, 40)}...
+                </Typography>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    padding: "5px",
+                  }}
                 >
-                  ${item?.price}
-                </Typography>
-                <Typography component={Button}>
-                  <ShoppingBasketIcon />
-                </Typography>
+                  <Typography
+                    variant="h5"
+                    component={"h4"}
+                    sx={{ color: "primary.main" }}
+                  >
+                    ${item?.price}
+                  </Typography>
+                  <Typography component={Button}>
+                    <ShoppingBasketIcon />
+                  </Typography>
+                </Box>
               </Box>
             </Box>
           </Grid>
