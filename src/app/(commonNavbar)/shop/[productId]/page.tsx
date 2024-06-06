cd6efc4662d8172ecce7194d0c64893b756e0b8c
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-06-06 18:44:02 +0600

    get single product by params success and fetch request writen

--- src/app/(commonNavbar)/shop/[productId]/page.tsx
+++ src/app/(commonNavbar)/shop/[productId]/page.tsx
@@ -1,6 +1,20 @@
 import { Container, Typography } from "@mui/material";
 
-const ProductDetailsPage = () => {
+type TProps = {
+  params: {
+    productId: string;
+  };
+};
+
+const ProductDetailsPage = async ({ params }: TProps) => {
+  console.log("product id", params?.productId);
+  const res = await fetch("/", {
+    method: "GET",
+    cache: "no-store",
+  });
+
+  const product = await res.json();
+
   return (
     <Container>
       <Typography>product details load here</Typography>
