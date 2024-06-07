acbc32ded7f96d3e4335319a225349401f4744a7
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-06-07 20:52:11 +0600

    view details and delete button icon added on white list page

--- src/app/(commonNavbar)/white-list/page.tsx
+++ src/app/(commonNavbar)/white-list/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { Box, Container, Typography } from "@mui/material";
-// table
+import { Box, Button, Container, Typography } from "@mui/material";
+
 import * as React from "react";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
@@ -13,6 +13,8 @@ import TableRow from "@mui/material/TableRow";
 import { useAppSelector } from "@/redux/hooks";
 import Image from "next/image";
 import Link from "next/link";
+import VisibilitySharpIcon from "@mui/icons-material/VisibilitySharp";
+import DeleteSharpIcon from "@mui/icons-material/DeleteSharp";
 
 interface Column {
   id: "images" | "items" | "price" | "actions";
@@ -52,77 +54,112 @@ const WhiteListPage = () => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
-  
+
   return (
     <Container>
       <Box>
-        <Typography variant="h5" component={"h4"} textAlign={"center"}>
+        <Typography
+          variant="h4"
+          component={"h5"}
+          textAlign={"center"}
+          my={6}
+          fontWeight={700}
+        >
           Your White Listed Products
         </Typography>
-        <Box>
-          <Paper sx={{ width: "100%", overflow: "hidden" }}>
-            <TableContainer sx={{ maxHeight: 440 }}>
-              <Table stickyHeader aria-label="sticky table">
-                <TableHead>
-                  <TableRow>
-                    {columns.map((column) => (
-                      <TableCell
-                        key={column.id}
-                        align={column.align}
-                        style={{ minWidth: column.minWidth }}
-                      >
-                        {column.label}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {products
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((product) => {
-                      return (
-                        <TableRow hover tabIndex={-1} key={product._id}>
-                          <>
-                            <TableCell>
-                              <Image
-                                src={product.image}
-                                alt="product image"
-                                width={40}
-                                height={40}
-                              />
-                            </TableCell>
-                            <TableCell>
-                              <Typography>{product.name}</Typography>
-                            </TableCell>
-                            <TableCell>
-                              <Typography>{product.price}</Typography>
-                            </TableCell>
-                            <TableCell>
-                              <Typography
-                                href={`/shop/${product._id}`}
-                                component={Link}
-                              >
-                                View Details
-                              </Typography>
-                            </TableCell>
-                          </>
-                        </TableRow>
-                      );
-                    })}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <TablePagination
-              rowsPerPageOptions={[10, 20, 50]}
-              component="div"
-              count={products.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-          </Paper>
-        </Box>
+        {products && products.length > 0 ? (
+          <Box>
+            <Paper sx={{ width: "100%", overflow: "hidden" }}>
+              <TableContainer sx={{ maxHeight: 440 }}>
+                <Table stickyHeader aria-label="sticky table">
+                  <TableHead>
+                    <TableRow>
+                      {columns.map((column) => (
+                        <TableCell
+                          key={column.id}
+                          align={column.align}
+                          style={{ minWidth: column.minWidth }}
+                        >
+                          {column.label}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {products
+                      .slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                      .map((product) => {
+                        return (
+                          <TableRow hover tabIndex={-1} key={product._id}>
+                            <>
+                              <TableCell>
+                                <Image
+                                  src={product.image}
+                                  alt="product image"
+                                  width={40}
+                                  height={40}
+                                />
+                              </TableCell>
+                              <TableCell>
+                                <Typography>{product.name}</Typography>
+                              </TableCell>
+                              <TableCell>
+                                <Typography>{product.price}</Typography>
+                              </TableCell>
+                              <TableCell>
+                                <Box
+                                  sx={{
+                                    display: "flex",
+                                  }}
+                                >
+                                  <Typography
+                                    href={`/shop/${product._id}`}
+                                    component={Link}
+                                  >
+                                    <Button>
+                                      <VisibilitySharpIcon />
+                                    </Button>
+                                  </Typography>
+                                  <Typography component={Button} ml={2}>
+                                    <DeleteSharpIcon />
+                                  </Typography>
+                                </Box>
+                              </TableCell>
+                            </>
+                          </TableRow>
+                        );
+                      })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <TablePagination
+                rowsPerPageOptions={[10, 20, 50]}
+                component="div"
+                count={products.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </Paper>
+          </Box>
+        ) : (
+          <Box>
+            <Typography
+              variant="h4"
+              component={"h5"}
+              textAlign={"center"}
+              my={6}
+              color={"red"}
+              fontWeight={500}
+            >
+              There Are No Products In the White List
+            </Typography>
+          </Box>
+        )}
       </Box>
     </Container>
   );
