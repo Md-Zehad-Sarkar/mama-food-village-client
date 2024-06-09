ea61309381ac0e074390609b2e74da254a04a7da
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-06-09 18:00:24 +0600

    cart slice add and add to cart added on click success

--- src/app/(commonNavbar)/shop/[productId]/components/AddToCartButton.tsx
+++ src/app/(commonNavbar)/shop/[productId]/components/AddToCartButton.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { addToCart } from "@/redux/features/cartSlice";
+import { useAppDispatch } from "@/redux/hooks";
 import { TProduct } from "@/types/products.type";
 import { Button } from "@mui/material";
 
 const AddToCartButton = ({ product }: { product: TProduct }) => {
-  const handleAddToCart = () => {
-    console.log("add to cart");
+  const dispatch = useAppDispatch();
+  const handleAddToCart = (product: TProduct) => {
+    dispatch(addToCart(product));
   };
-  return <Button onClick={handleAddToCart}>Add To Cart</Button>;
+  return <Button onClick={() => handleAddToCart(product)}>Add To Cart</Button>;
 };
 
 export default AddToCartButton;
--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -2,8 +2,7 @@ import { combineReducers } from "@reduxjs/toolkit";
 import baseApi from "./api/baseApi";
 import userSlice from "./features/userSlice";
 import whiteListSlice from "./features/whiteListSlice";
-
-
+import cartSlice from "./features/cartSlice";
 
 // export const rootReducer = {
 //   [baseApi.reducerPath]: baseApi.reducer,
@@ -14,4 +13,5 @@ export const rootReducer = combineReducers({
   [baseApi.reducerPath]: baseApi.reducer,
   user: userSlice,
   whiteListProducts: whiteListSlice,
+  carts: cartSlice,
 });
